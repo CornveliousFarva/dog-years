38818bd47e32422456b27ee314fea6bd7128da79
Greg Schuman <gcschuman@hotmail.com>
2022-07-27 13:28:10 -0700

    Added the subtraction for laterYears, the variables for my age in dog years, my name, and the console log that runs it all.

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ earlyYears *= 10.5;
 
 // Subtracting myAge by 2. That will be the new variable laterYears
 let laterYears = myAge - 2;
+
+// Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+
+// Adding earlyYears and laterYears to make a new variable called myAgeInDogYears
+const myAgeInDogYears = earlyYears * laterYears
+
+const myName = 'Harper'
+
+'Harper'.toLowerCase();
+
+console.log(`My name is ${myName}.  I am ${myAgeInDogYears} years old in dog years`)
+
+
+
