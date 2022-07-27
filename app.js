ea625b0f8719dbb864fa8da84366f6c6e521e3f9
Greg Schuman <gcschuman@hotmail.com>
2022-07-27 13:09:55 -0700

    Initial commit: initial definition of variables.

---
+++ app.js
@@ -0,0 +1,11 @@
+// Variable defining my age to start. It is a const because it will not be changing
+const myAge = 32;
+
+// Variable defining early years. It's a let because it will be changing
+let earlyYears = 2;
+
+// Multiplying earlyYears by 10.5
+earlyYears *= 10.5;
+
+// Subtracting myAge by 2. That will be the new variable laterYears
+let laterYears = myAge - 2;
